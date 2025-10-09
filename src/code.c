12c76ad55a78531b8d50cfc947d519db6c263d56
Willd231 <dellinger.w3@gmail.com>
2025-10-08 20:18:30 -0400

    F

--- src/code.c
+++ src/code.c
@@ -4,86 +4,78 @@
 #include <stdlib.h>
 
 
-
-
-float *  getTemps(){
-
-
-	char * path = "/sys/class/thermal/thermal_zone";
+char ** get_temps(){
+	char line[1024];
+	int capacity = 8;
+	char ** array = malloc(sizeof(char*) * capacity);
+	int index = 0;
+	FILE * fp = popen("cat /sys/class/thermal/thermal_zone*/temp", "r"); 
+	while(fgets(line, sizeof(line), fp)){
+		line[strcspn(line, "\n")] = 0;
+		if(index >= capacity){
+			capacity *=2;
+			array = realloc(array, capacity * sizeof(char*));
+		}
+		array[index] = strdup(line);	
+		index++;
+	}
+	array[index+1] = NULL;
+fclose(fp);
+return array;
 	
-	char * temp = "/temp";
-	char ** string = malloc(sizeof(char*) * 9);
-	float * temps = malloc(sizeof(int) * 9); 
+}
+
+char ** get_types(){
+	char line[1024];
+	int capacity = 8;
+	char ** array = malloc(sizeof(char*) * capacity);
+	int index = 0;
+	FILE * fp = popen("cat /sys/class/thermal/thermal_zone*/type", "r"); 
+	while(fgets(line, sizeof(line), fp)){
+		line[strcspn(line, "\n")] = 0;
+		if(index >= capacity){
+			capacity *=2;
+			array = realloc(array, capacity * sizeof(char*));
+		}
+		array[index] = strdup(line);	
+		index++;
+	}
+	array[index+1] = NULL;
+fclose(fp);
+return array;
 	
-	//char * final = malloc(sizeof(char) * strlen(path) + strlen(temp) + 2);
-	for(int i = 0; i < 9; i++){
+}
+char ** get_mem(){
+	char line[1024];
+	int capacity = 8;
+	char ** array = malloc(sizeof(char*) * capacity);
+	int index = 0;
+	FILE * fp = popen("cat /proc/meminfo", "r"); 
+	while(fgets(line, sizeof(line), fp)){
+		line[strcspn(line, "\n")] = 0;
+		if(index >= capacity){
+			capacity *=2;
+			array = realloc(array, capacity * sizeof(char*));
+		}
+		if( (strstr(line,"MemTotal"))||
+				 (strstr(line,"MemTotal"))||
+				(strstr(line,"MemTotal"))||
+				(strstr(line,"MemTotal"))){
+				array[index] = strdup(line);	
 				
-		char buffer[10];
-		snprintf(buffer,sizeof(buffer), "%d", i);
-		//printf("%s\n", buffer);
-		string[i] = (char*)malloc(sizeof(char) * (strlen(path) + strlen(temp) + strlen(buffer) + 1));
-		strcpy(string[i], path);
-		strcat(string[i], buffer);
-		strcat(string[i], temp);
-		//printf("%s\n", string[i]);
-																
-	 //the segfault is due to trying to use fread on a NULL or nonexistant path aka : path
-
-	
-	FILE * fp;
-	if((fp = fopen(string[i], "r")) == NULL){
-		printf("File not found\n");
-		return NULL;
+		}array[index] = strdup(line);	
+		index++;
 	}
-	else{
-		fp = fopen(string[i], "r");
-	} 
-	
-	//fread(path, 4, 1, fp); 
-	fscanf(fp, "%f", &temps[i]);
-			
-	for(int i = 0; i < 9; i++){
-		if(temps[i]!= 0){
-			temps[i] = temps[i];
-		}
-	}
-
-		
-		}
-		return temps;
-}
-
-
-int get_Meminfo(){
-
-	FILE * fp;
-	if((fp = fopen("/proc/meminfo", "r")) == NULL){
-		printf("memfile not found \n"); 
-		return -2;
-	};
-	
-		int value;
-		char line[100];
-		int line_num = 0;
-		fp = fopen("/proc/meminfo", "r");
-		//while(fgets(line, sizeof(line), fp)){
-		//	line_num++;
-		//	if(line_num == 2){
-		//		sscanf(line, "%d", &value);
-		fscanf(fp, "%s", line);
-				printf("%s", line);
-				//break;
-			//}
-		//}
-	return value;
+	array[index+1] = NULL;
+fclose(fp);
+return array;
 	
 }
-
 
 
 int main(){
 	
-	while(1==1){
+	/*while(1==1){
 		if(getTemps() == NULL){
 			perror("Error getting temperatures\n");
 			return -2;
@@ -97,14 +89,26 @@ int main(){
 		printf("\rCPU TEMP: %.1lf°C \t\t\tWIFI CHIP TEMP: %.1lf°C\tMemory Usage: %d", temps[7]/1000, temps[8]/1000, mem);
 		fflush(stdout);
 		//printf("\rWIFI Chip: %f°C", temps[8]);
-				
+			
 		
 		sleep(1);
 		}
 		
-		
-	
+*/
+char ** array = get_types();
+for(int i =0; array[i]!=NULL; i++){
+	printf("%s\n", array[i]);
+}	
+char ** array1 = get_temps();
+for(int i =0; array1[i]!=NULL; i++){
+	printf("%s\n", array1[i]);
+}	
+char ** array2 = get_mem();
+for(int i =0; array1[i]!=NULL; i++){
+	printf("%s\n", array2[i]);
+}	
+
 	
 	return 0;
 }
-\
+
